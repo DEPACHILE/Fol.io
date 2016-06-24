69690eb5316f079eb1982881e20caa040272c3b1
jorge pinto <jorge pinto>
2016-06-24 19:59:01 -0300

    conexion con servidor

--- app/src/main/java/cl/cadcc/folio/MainActivity.java
+++ app/src/main/java/cl/cadcc/folio/MainActivity.java
@@ -4,20 +4,43 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cl.cadcc.folio.api.ApiHttps;
+import cl.cadcc.folio.fragments.ServerConnectFragment;
+import cl.cadcc.folio.fragments.WelcomeFragment;
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.HttpStatus;
+
+public class MainActivity extends AppCompatActivity implements WelcomeFragment.OnFragmentInteractionListener {
 
     private BroadcastReceiver nfcChangedReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fragment welcomeFragment = new WelcomeFragment();
         setContentView(R.layout.activity_main);
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        t.replace(R.id.folio_fragment,welcomeFragment , "welcome");
+        t.commit();
     }
 
     @Override
@@ -45,11 +68,11 @@ public class MainActivity extends AppCompatActivity {
         if (tuiTextView == null) return;
 
         if (nfcAdapter == null) {
-            tuiTextView.setText("NFC is not available");
+            tuiTextView.setText("Lo sentimos, tu celular no tiene NFC.");
         } else if (!nfcAdapter.isEnabled()) {
-            tuiTextView.setText("NFC is disabled");
+            tuiTextView.setText("NFC está deshabilitado, habilítalo, por favor.");
         } else {
-            tuiTextView.setText("Waiting to read");
+            tuiTextView.setText("Acerca la tarjeta, por favor.");
         }
     }
 
@@ -81,13 +104,62 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        TextView tuiTextView = (TextView) findViewById(R.id.textView_tui);
-                        if (tuiTextView == null) return;
-                        tuiTextView.setText("Card ID: " + cardId);
+                        Fragment serverConnectFragment = new ServerConnectFragment();
+                        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+                        t.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                        t.replace(R.id.folio_fragment,serverConnectFragment , "connect");
+                        t.commit();
+                        getSupportFragmentManager().executePendingTransactions();
+                        TextView nfcValue = (TextView) findViewById(R.id.nfc_value);
+                        if (nfcValue == null) {
+                            Log.d("Adderou", "Es nulo!");
+                            return;
+                        } else {
+                            nfcValue.setText("Card ID: " + cardId.substring(2, cardId.length()).toUpperCase());
+                            String tuiId=cardId.substring(2, cardId.length()).toUpperCase();
+                            try {
+                                Log.d("testTUID",tuiId);
+                                findUser(tuiId);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
                 });
             }
         }, flags, null);
+    }
+
+
+    public void findUser(String tuiId) throws JSONException {
+        ApiHttps.get("/vote/1/1/"+tuiId+"/1", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("testAPISUCCESS",response.toString());
+                Toast.makeText(getApplicationContext(),response.toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+                // Pull out the first event on the public timeline
+                JSONObject firstEvent = null;
+                String tweetText = null;
+                try {
+                    firstEvent = (JSONObject) timeline.get(0);
+                    tweetText = firstEvent.getString("text");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                // Do something with the response
+                Log.d("testAPI",tweetText);
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("testAPI","Error");
+            }
+        });
     }
 
     public void stopNfcReader() {
